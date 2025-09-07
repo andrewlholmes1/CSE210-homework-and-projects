d2ca207639186bd1218698b9115f87ce17664503
littleandy667 <aholmes1@byupathway.edu>
2025-09-07 15:44:26 -0700

    Created a job and resume cs files, then complies all the data on program cs to show 2 jobs and the job title

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -1,9 +1,31 @@
 using System;
+using System.Threading.Tasks.Dataflow;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Firefly Studios";
+        job1._startYear = "2020";
+        job1._endYear = "2023";
+
+        Job job2 = new Job();
+        job2._jobTitle = "Software Engineer";
+        job2._company = "Bethesda Softworks";
+        job2._startYear = "2023";
+        job2._endYear = "2024";
+
+        Resume myResume = new Resume();
+        myResume._name = "Andrew Holmes";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
+
+
+
     }
 }
