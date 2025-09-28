84ec7ef90ff7f229ecbc3760fe705d5f7390bd25
littleandy667 <aholmes1@byupathway.edu>
2025-09-28 12:19:14 -0700

    Created Homework program, this collects all assignment from history, algebra, and english literature

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Homework Project.");
+        Assignment a1 = new Assignment("John Doe", "History on historal wars");
+        Console.WriteLine(a1.GetSummary());
+
+
+
+        MathAssignment a2 = new MathAssignment("Jane Smith", "Algebra", "Section 2.3", "1-10");
+        Console.WriteLine(a2.GetSummary());
+        Console.WriteLine(a2.GetHomeworkList());
+
+        WritingAssignment a3 = new WritingAssignment("Emily Johnson", "English Literature", "The Great Gatsby");
+        Console.WriteLine(a3.GetSummary());
+        Console.WriteLine(a3.GetWritingInformation());
     }
 }
